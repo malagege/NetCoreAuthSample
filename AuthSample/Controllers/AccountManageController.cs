bc5bb3f10c6e5a79bd5029f8bcd15ef15ecd6663
malagege <s9714020@gmail.com>
2022-08-17 18:25:20 +0800

    AccountManage 加入授權設定

---
+++ AuthSample/Controllers/AccountManageController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthSample.Controllers
+{
+    [Authorize]
+    public class AccountManageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
