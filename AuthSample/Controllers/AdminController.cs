578bdd61e7e5c719a2a79da9c27cc8d3c5a32516
malagege <s9714020@gmail.com>
2022-08-19 11:18:52 +0800

    建立角色

---
+++ AuthSample/Controllers/AdminController.cs
@@ -0,0 +1,55 @@
+﻿using AuthSample.ViewModels.Admin;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AuthSample.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRoleAsync(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //角色 Name 不能重覆
+                var identityRole = new IdentityRole
+                {
+                    Name = model.RoleName,   
+                };
+
+                IdentityResult result = await _roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+
+                foreach(IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
+    }
+}
