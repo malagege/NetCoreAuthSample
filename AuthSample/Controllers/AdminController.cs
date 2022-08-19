6f56c8bad3b7f0a9eee8733474873a6a49ec8bfa
malagege <s9714020@gmail.com>
2022-08-19 17:36:34 +0800

    編輯角色_修改名稱

--- AuthSample/Controllers/AdminController.cs
+++ AuthSample/Controllers/AdminController.cs
@@ -1,7 +1,10 @@
-﻿using AuthSample.ViewModels.Admin;
+﻿using AuthSample.Models;
+using AuthSample.ViewModels.Admin;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,10 +14,12 @@ namespace AuthSample.Controllers
     public class AdminController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public AdminController(RoleManager<IdentityRole> roleManager)
+        public AdminController(RoleManager<IdentityRole> roleManager,UserManager<ApplicationUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -60,6 +65,68 @@ namespace AuthSample.Controllers
         {
             IQueryable<IdentityRole> roles = _roleManager.Roles;
             return View(roles);
+        }
+        #endregion
+
+        # region 編輯角色
+        [HttpGet]
+        public async Task<IActionResult> EditRoleAsync(string id)
+        {
+            // 通過參數 id 帶入 roleManager 查詢
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            if( role == null)
+            {
+                ViewBag.ErrorMessage = $"角色ID:{id}不存在";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name,
+            };
+            IList<ApplicationUser> users = await _userManager.Users.ToListAsync();
+
+            foreach (var user in users)
+            {
+                // 每個使用者的 role 判斷是否有權限
+                if(await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            IdentityRole role =  await _roleManager.FindByIdAsync(model.Id);
+
+            if(role == null)
+            {
+                ViewBag.ErrorMessage = $"角色Id={model.Id}不存在";
+                return View("NotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+
+                var result = await _roleManager.UpdateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("RoleList");
+                }
+                
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model);
+            }
+
         }
         #endregion
     }
---
+++ AuthSample/ViewModels/Admin/EditRoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AuthSample.ViewModels.Admin
+{
+    public class EditRoleViewModel
+    {
+        public string Id { get; set; }
+        public string RoleName { get; set; }
+        public IList<string> Users { get; set; }
+    }
+}
