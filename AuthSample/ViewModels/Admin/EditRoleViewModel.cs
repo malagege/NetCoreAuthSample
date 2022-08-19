4ab38281d9998a8d5a1b4cb28441e026efd2a937
malagege <s9714020@gmail.com>
2022-08-19 17:53:21 +0800

    初始化使用者清單

--- AuthSample/ViewModels/Admin/EditRoleViewModel.cs
+++ AuthSample/ViewModels/Admin/EditRoleViewModel.cs
@@ -4,6 +4,11 @@ namespace AuthSample.ViewModels.Admin
 {
     public class EditRoleViewModel
     {
+        public EditRoleViewModel()
+        {
+            // 初始化使用者清單
+            Users = new List<string>();
+        }
         public string Id { get; set; }
         public string RoleName { get; set; }
         public IList<string> Users { get; set; }
