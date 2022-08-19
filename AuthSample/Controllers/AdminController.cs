2a21605f18577963372acad4092ecec1ba5c88a4
malagege <s9714020@gmail.com>
2022-08-20 01:56:15 +0800

    使用者添加角色

--- AuthSample/Controllers/AdminController.cs
+++ AuthSample/Controllers/AdminController.cs
@@ -129,5 +129,100 @@ namespace AuthSample.Controllers
 
         }
         #endregion
+
+        #region 角色加入使用者設定
+        public async Task<IActionResult> EditUserInRoleAsync(string roleId)
+        {
+            ViewBag.roleId = roleId;
+
+            IdentityRole role = await _roleManager.FindByIdAsync(roleId);
+            if(role == null)
+            {
+                ViewBag.ErrorMessage = $"角色{roleId}不存在";
+                return View("NotFound");
+            }
+            var model = new List<UserRoleViewModel>();
+            var users = await _userManager.Users.ToListAsync();
+            foreach (var user in users)
+            {
+                var userRoleViewModel = new UserRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                };
+                // 判斷使用者有無此角色
+                // 注意第二個參數是 role.Name
+                var isInRole = await _userManager.IsInRoleAsync(user, role.Name);
+
+                if (isInRole)
+                {
+                    userRoleViewModel.IsSelected = true;
+                }
+                else
+                {
+                    userRoleViewModel.IsSelected = false;
+                }
+                model.Add(userRoleViewModel);
+            }
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUserInRoleAsync(IList<UserRoleViewModel> model, string roleId)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(roleId);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"角色id={roleId}不存在";
+                return View("NotFound");
+            }
+
+            for(int i=0; i<model.Count; i++)
+            {
+                ApplicationUser user = await _userManager.FindByIdAsync(model[i].UserId);
+
+                IdentityResult result;
+                // ***重點***  Start
+                // 選擇跟查詢結果不一樣，更改資料
+                bool isInRole = await _userManager.IsInRoleAsync(user, role.Name);
+                if (model[i].IsSelected && !(isInRole))
+                {
+                    result = await _userManager.AddToRoleAsync(user, role.Name);   // 使用者角色新增 role.Name
+                }
+                else if (!model[i].IsSelected && (isInRole))
+                { 
+                    result = await _userManager.RemoveFromRoleAsync(user, role.Name);   // 使用者角色移除 role.Name
+                }
+                else
+                {
+                    continue;     //與資料庫一樣不做處理
+                }
+
+                if (result.Succeeded)
+                {
+                    if(i < (model.Count - 1))  //簡單來說，第一次和中間次數跑 continue 執行最後一次會跑 else 內容
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return RedirectToAction("EditRole", new { Id = roleId });
+                    }
+                }
+                // ***重點***  End
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+
+            //return View(model); 這邊要指定一個 roleId
+            return RedirectToAction("EditRole", new { Id = roleId });
+        }
+
+        #endregion
     }
 }
---
+++ AuthSample/ViewModels/Admin/UserRoleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthSample.ViewModels.Admin
+{
+    public class UserRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
