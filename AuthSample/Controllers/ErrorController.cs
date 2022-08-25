9caa3e3a20e952b0642f14a2963c40b066460ea5
malagege <s9714020@gmail.com>
2022-08-25 11:25:26 +0800

    錯誤狀態處理

--- AuthSample/Controllers/ErrorController.cs
+++ AuthSample/Controllers/ErrorController.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 
 namespace AuthSample.Controllers
 {
+    [AllowAnonymous]
     public class ErrorController : Controller
     {
         private readonly ILogger _logger;
@@ -14,7 +15,26 @@ namespace AuthSample.Controllers
             _logger = logger;
         }
 
-        [AllowAnonymous]
+        //使用屬性路由，如果狀態代碼為404，則路徑將變為Error/404
+        [Route("Error/{statusCode}")]
+        [HttpGet]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            var statusCodeResult =
+                HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "抱歉，你訪問的頁面不存在";
+                    //LogWarning() 方法將異常記錄作為日誌中的警告類別記錄
+                    _logger.LogWarning($"發生了一個404錯誤. 路徑 = " +
+                $"{statusCodeResult.OriginalPath} 以及查詢字符串 = " +
+                $"{statusCodeResult.OriginalQueryString}");
+                    break;
+            }
+            return View("NotFound");
+        }
+
         public IActionResult Error()
         {
             // 獲取異常詳細資訊
